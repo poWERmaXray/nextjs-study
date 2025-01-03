fd39f479e3d7483fe5852d3a25f6bea05d39a030
张伟 <speedcrusade0983@163.com>
2025-01-03 18:24:42 +0800

    [fix][font]

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
-import { Inter, JetBrains_Mono } from "next/font/google"
+import { Inter, JetBrains_Mono, Lusitana } from "next/font/google"
 export const inter = Inter({ subsets: ["latin"]});
 export const jetbrainsMono = JetBrains_Mono({subsets: ["latin"], variable: "--font-mono"});
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
